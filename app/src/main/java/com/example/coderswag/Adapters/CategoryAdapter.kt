56a81be11e41d08425103e19685cb5cd8c9ddb44
tonymac137 <tonymac137@gmail.com>
2021-02-08 16:21:50 +0000

    V1.0.002

---
+++ app/src/main/java/com/example/coderswag/Adapters/CategoryAdapter.kt
@@ -0,0 +1,67 @@
+package com.example.coderswag.Adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.LayoutInflater.*
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.coderswag.Model.Category
+import com.example.coderswag.R
+import com.example.coderswag.R.layout
+import com.example.coderswag.databinding.ActivityMainBinding
+
+class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapter(){
+
+    val context = context
+    val categories = categories
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+
+        val categoryView: View
+        val holder : ViewHolder
+
+        if (convertView == null) {
+            categoryView = from(context).inflate(layout.category_list_item, null)
+            holder = ViewHolder()
+            holder.categoryImage = categoryView.findViewById(R.id.categoryImage)
+            holder.categoryName = categoryView.findViewById(R.id.categoryName)
+
+            categoryView.tag = holder
+        }else {
+            holder = convertView.tag as ViewHolder
+            categoryView = convertView
+        }
+
+        val category = categories[position]
+        val resourceID = context.resources.getIdentifier(category.image, "drawable", context.packageName)
+        holder.categoryImage?.setImageResource(resourceID)
+
+        holder.categoryName?.text = category.title
+
+        return categoryView
+    }
+
+    override fun getCount(): Int {
+        return categories.count()
+    }
+
+    override fun getItem(position: Int): Any {
+        return categories[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    private class ViewHolder {
+        var categoryImage: ImageView? = null
+        var categoryName: TextView? = null
+
+    }
+
+}
--- app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
@@ -7,6 +7,7 @@ package com.example.coderswag.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.example.coderswag.Adapters.CategoryAdapter
 import com.example.coderswag.Model.Category
 import com.example.coderswag.R
 import com.example.coderswag.Services.DataService
@@ -19,19 +20,18 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     // Initialise the Adapter for the listView - using Model.Category
-    lateinit var adapter : ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
         // Create the Adapter for the listView
-        adapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1,
-            DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
+
 
         // Tell the listView who it needs to listen to
         binding.categoryListView.adapter = adapter
