06b268775f3722f99a8e4dd72395fec7fa066f09
tonymac137 <tonymac137@gmail.com>
2021-02-08 13:16:20 +0000

    V1.0.002

--- app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.example.coderswag.Controller
 
+// Kotlin for Android Beginner to Advanced
+// Udemy
+// Started 07/02/2021
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.coderswag.Model.Category
 import com.example.coderswag.R
+import com.example.coderswag.Services.DataService
+import com.example.coderswag.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    // Initialise the Adapter for the listView - using Model.Category
+    lateinit var adapter : ArrayAdapter<Category>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Create the Adapter for the listView
+        adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            DataService.categories)
+
+        // Tell the listView who it needs to listen to
+        binding.categoryListView.adapter = adapter
+
     }
+
+
+
 }
