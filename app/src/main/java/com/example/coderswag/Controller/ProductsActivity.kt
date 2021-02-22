a655d58fcbe01834a974d1ddfca4a7d1107a9f39
tonymac137 <tonymac137@gmail.com>
2021-02-22 20:47:38 +0000

    Leson-08

--- app/src/main/java/com/example/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/ProductsActivity.kt
@@ -1,14 +1,18 @@
 package com.example.coderswag.Controller
 
+import android.content.res.Configuration
 import android.nfc.cardemulation.CardEmulation.EXTRA_CATEGORY
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.coderswag.databinding.ActivityMainBinding
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.coderswag.Adapters.ProductsAdapter
+import com.example.coderswag.Services.DataService
 import com.example.coderswag.databinding.ActivityProductsBinding
 
 class ProductsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityProductsBinding
+    lateinit var adapter: ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +20,28 @@ class ProductsActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+        adapter = categoryType?.let { DataService.getProducts(it) }?.let {
+            ProductsAdapter(this,
+                it
+            )
+        }!!
+
+        var spanCount = 3
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            spanCount = 5
+        }
+
+        val screenSize = resources.configuration.screenWidthDp
+        if (screenSize > 720 && orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            spanCount = 7
+        }
+        if (screenSize > 720 && orientation == Configuration.ORIENTATION_PORTRAIT) {
+            spanCount = 5
+        }
+
+        val layoutManager = GridLayoutManager(this, spanCount)
+        binding.productsListView.layoutManager = layoutManager
+        binding.productsListView.adapter = adapter
     }
 }
--- app/src/main/java/com/example/coderswag/Services/DataService.kt
+++ app/src/main/java/com/example/coderswag/Services/DataService.kt
@@ -3,7 +3,7 @@ package com.example.coderswag.Services
 import com.example.coderswag.Model.Category
 import com.example.coderswag.Model.Product
 
-object DataService {
+object  DataService {
     val categories = listOf(
         Category("SHIRTS", "shirtimage"),
         Category("HOODIES", "hoodieimage"),
@@ -19,10 +19,18 @@ object DataService {
         Product("Devslopes Graphic Beanie", "£18", "hat1"),
         Product("Devslopes Hat Black", "£20", "hat2"),
         Product("Devslopes Hat White", "£18", "hat3"),
+        Product("Devslopes Hat Snapback", "£22", "hat4"),
+        Product("Devslopes Graphic Beanie", "£18", "hat1"),
+        Product("Devslopes Hat Black", "£20", "hat2"),
+        Product("Devslopes Hat White", "£18", "hat3"),
         Product("Devslopes Hat Snapback", "£22", "hat4")
     )
 
     val hoodies = listOf(
+        Product("Devslopes Hoodie Gray", "£28", "hoodie1"),
+        Product("Devslopes Hoodie Red", "£32", "hoodie2"),
+        Product("Devslopes Gray Hoodie", "£28", "hoodie3"),
+        Product("Devslopes Black Hoodie", "£28", "hoodie4"),
         Product("Devslopes Hoodie Gray", "£28", "hoodie1"),
         Product("Devslopes Hoodie Red", "£32", "hoodie2"),
         Product("Devslopes Gray Hoodie", "£28", "hoodie3"),
@@ -32,6 +40,11 @@ object DataService {
     )
 
     val shirts = listOf(
+        Product("Devslopes Shirt Black", "£18", "shirt1"),
+        Product("Devslopes Badge Light Gray", "£20", "shirt2"),
+        Product("Devslopes Logo Shirt Red", "£22", "shirt3"),
+        Product("Devslopes Hustle", "£22", "shirt4"),
+        Product("Kickflip Studios", "£18", "shirt5"),
         Product("Devslopes Shirt Black", "£18", "shirt1"),
         Product("Devslopes Badge Light Gray", "£20", "shirt2"),
         Product("Devslopes Logo Shirt Red", "£22", "shirt3"),
